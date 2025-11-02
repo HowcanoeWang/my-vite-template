dbe50171243f028ee338fd7688686cf71e28e19a
浩瀚猫 <howcanoewang@gmail.com>
2025-11-02 19:25:23 +0900

    allow sub pages directory

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // 如果环境变量存在，则使用它，否则默认为根目录
+  base: process.env.VITE_BASE_PATH || '/', 
   plugins: [
     tailwindcss(),
   ],
